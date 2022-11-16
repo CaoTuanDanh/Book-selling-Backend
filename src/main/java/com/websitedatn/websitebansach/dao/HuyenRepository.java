5ebb2fbe29a385fbcf84d8b6b749f8cd5b277fcb
CaoTuanDanh <115147816+CaoTuanDanh@users.noreply.github.com>
2022-11-16 11:04:33 +0700

    Update HuyenRepository.java

--- src/main/java/com/websitedatn/websitebansach/dao/HuyenRepository.java
+++ src/main/java/com/websitedatn/websitebansach/dao/HuyenRepository.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin("https://astonishing-sunburst-3df6f9.netlify.app")
+@CrossOrigin("https://book-selling-dtshop.netlify.app")
 public interface HuyenRepository extends JpaRepository<Huyen, Integer> {
     @Query("SELECT p FROM Huyen p WHERE CONCAT(p.tinhthanh_id) LIKE %?1%")
     public List<Huyen> findAllById(String keyword);
