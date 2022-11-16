50e88abd5e22779802a828c15272e33724a660d2
CaoTuanDanh <115147816+CaoTuanDanh@users.noreply.github.com>
2022-11-16 11:07:32 +0700

    Update AddressRepository.java

--- src/main/java/com/websitedatn/websitebansach/purchase_dao/AddressRepository.java
+++ src/main/java/com/websitedatn/websitebansach/purchase_dao/AddressRepository.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
-@CrossOrigin("https://astonishing-sunburst-3df6f9.netlify.app")
+@CrossOrigin("https://book-selling-dtshop.netlify.app")
 public interface AddressRepository extends JpaRepository<Address,Long> {
 
     @Query("SELECT p FROM Address p WHERE CONCAT(p.order_id) LIKE %?1%")
