a9c189742c551423966b1581cd01d5274447ff62
CaoTuanDanh <115147816+CaoTuanDanh@users.noreply.github.com>
2022-11-16 11:05:31 +0700

    Update TinhThanhVNRepository.java

--- src/main/java/com/websitedatn/websitebansach/dao/TinhThanhVNRepository.java
+++ src/main/java/com/websitedatn/websitebansach/dao/TinhThanhVNRepository.java
@@ -4,7 +4,7 @@ import com.websitedatn.websitebansach.entity.TinhThanhVN;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("https://astonishing-sunburst-3df6f9.netlify.app")
+@CrossOrigin("https://book-selling-dtshop.netlify.app")
 public interface TinhThanhVNRepository extends JpaRepository<TinhThanhVN, Integer> {
 
 }
